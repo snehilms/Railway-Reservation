007a82f67f84bfeedd65d4a238c5de6760fd103c
kannanabhi <65603853+kannanabhi@users.noreply.github.com>
2020-06-08 22:28:33 +0530

    partially done my part
    will do more improvement today overnight on writing on file and how to searching from the file.thanks

--- structs_and_menu.cpp
+++ structs_and_menu.cpp
@@ -1,34 +1,30 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include<stdio.h>
 #include<stdlib.h>
-
-
-int
-main ()
-{
-    struct train{
+#include<string.h>
+void bookaticket();
+int findmaxpnrno();
+ struct train
+    {
 	char source[50];
 	char dest[50];
 	int train_num;
 	int seats_left;
-};
+    };
 struct ticket
-{
-   char passenger_name[50]; 
+   {
+   char passenger_name[50];
    int pnr;
    int train_num;
-   
-};
 
-  printf ("\t\t=================================================\n");
-  for(int i=0;i<=33126000;i++);
+   };
+
+
+int main ()
+{
+   
+
+ printf ("\t\t=================================================\n");
+ for(int i=0;i<=33126000;i++);
   printf ("\t\t|                                               |\n");
   for(int i=0;i<=33126000;i++);
   printf ("\t\t|        -----------------------------          |\n");
@@ -45,7 +41,7 @@ struct ticket
   for(int i=0;i<=33126000;i++);
   printf ("\t\t|              	 CREATED BY                     |\n");
   for(int i=0;i<=33126000;i++);
-  printf ("\t\t|        Abhijeet, Snehil and Pranshu           |\n");
+  printf ("\t\t|        Abhijith, Snehil and Pranshu           |\n");
   for(int i=0;i<=33126000;i++);
   printf ("\t\t|                                               |\n");
   for(int i=0;i<=33126000;i++);
@@ -83,6 +79,7 @@ struct ticket
     case 1:
       // reservation function
       printf ("\t\tchoice 1 selected");
+      bookaticket();
       break;
     case 2:
       // viewdetails function
@@ -107,4 +104,46 @@ struct ticket
     }
 
   return (0);
+}
+void bookaticket()
+{
+    int buffer;char buff[40];
+    FILE *fp,*gp;
+    fp=fopen("textfileB","a+");
+    struct ticket a;
+    printf("enter the number of seats to be booked");
+    scanf("%d",&x);
+    printf("Enter the train number");
+    fgets(buffer,10,stdin);
+    //use the enquiry function from the question 5
+    //check if x is less than the available seats
+    if(x</*available no of seats*/)
+    {
+
+    printf("enter the boarding point");
+    printf("enter the destination point");
+    int prno=findmaxpnrno();
+    for(int i=0;i<x;i++)
+       {
+         printf("Enter the name of the passenger");
+         fgets(buff[i][40],40,stdin);
+         hisprno=++prno;
+         fputs(buffer,fp);//input all the data into the file.
+         fputs(buff[i][40],fp);
+         fputs(hisprno,fp);
+         //decrease the number of seats left by 1
+       }
+    }
+    else
+        printf("sorry seats are already booked check another train");
+
+}
+int findmaxpnrno()
+{    file fp;
+     fp=fopen("textfileB","r");
+
+     //textfileB is having the list of all the tickets booked so far
+     //now we have to read through the text file and find the biggest pnr no so far
+     //return that pnr no
+    return prnmax;
 }
